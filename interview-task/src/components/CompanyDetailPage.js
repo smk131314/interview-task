e28fc687e846a3a3947bb7673a2be03dfc5a7029
Jade.Kim <jade.kim@travelflan.com>
2022-04-03 12:00:14 +0100

    style: change overview card placement

--- interview-task/src/components/CompanyDetailPage.js
+++ interview-task/src/components/CompanyDetailPage.js
@@ -19,16 +19,18 @@ const CompanyDetailPage = () => {
       <main>
         <a className={styles.backButton}><img src='/images/arrow_back.svg' alt='back icon'/><span>Back</span></a>
         <SummarySection CompanyInfo={COMPANY_SUMMARY} />
-        <NavTabs selectedTab={selectedTab} onTabClick={setSelectedTab} tabList={TAB_LIST}/>
-        <section className={styles.cardsSection}>
-          <TabItem tabName={TAB_LIST[0]} selectedTab={selectedTab}><StockChart /></TabItem>
-          <TabItem tabName={TAB_LIST[1]} selectedTab={selectedTab}><p>{TAB_LIST[1]}</p></TabItem>
-          <TabItem tabName={TAB_LIST[2]} selectedTab={selectedTab}><p>{TAB_LIST[2]}</p></TabItem>
-          <TabItem tabName={TAB_LIST[3]} selectedTab={selectedTab}><p>{TAB_LIST[3]}</p></TabItem>
-          <TabItem tabName={TAB_LIST[4]} selectedTab={selectedTab}><p>{TAB_LIST[4]}</p></TabItem>
-          <TabItem tabName={TAB_LIST[5]} selectedTab={selectedTab}><p>{TAB_LIST[5]}</p></TabItem>
-          <div className={styles.fixedCard}>
-            <RoundCard isSmall title={'Overview'}><InfoTable infoObj={OVERVIEW_INFO}/></RoundCard>
+        <RoundCard isSmall title={'Overview'}><InfoTable infoObj={OVERVIEW_INFO}/></RoundCard>
+        <section className={styles.bottomSection}>
+          <div className={styles.cardTabsWrapper}>
+            <NavTabs selectedTab={selectedTab} onTabClick={setSelectedTab} tabList={TAB_LIST}/>
+            <TabItem tabName={TAB_LIST[0]} selectedTab={selectedTab}><StockChart /></TabItem>
+            <TabItem tabName={TAB_LIST[1]} selectedTab={selectedTab}><p>{TAB_LIST[1]}</p></TabItem>
+            <TabItem tabName={TAB_LIST[2]} selectedTab={selectedTab}><p>{TAB_LIST[2]}</p></TabItem>
+            <TabItem tabName={TAB_LIST[3]} selectedTab={selectedTab}><p>{TAB_LIST[3]}</p></TabItem>
+            <TabItem tabName={TAB_LIST[4]} selectedTab={selectedTab}><p>{TAB_LIST[4]}</p></TabItem>
+            <TabItem tabName={TAB_LIST[5]} selectedTab={selectedTab}><p>{TAB_LIST[5]}</p></TabItem>
+          </div>
+          <div className={styles.fixedCardWrapper}>
             <RoundCard isSmall title={'Key information'}><InfoTable isVertical infoObj={KEY_INFO}/></RoundCard>
           </div>
         </section>
