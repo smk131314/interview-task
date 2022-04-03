a35e15d15956ddf00dfe3937fdcc1d79b82cecbe
Jade.Kim <jade.kim@travelflan.com>
2022-04-03 13:35:48 +0100

    style: fix chart container height

--- interview-task/src/components/StockChart.js
+++ interview-task/src/components/StockChart.js
@@ -5,9 +5,7 @@ import styles from '../styles/StockChart.module.scss'
 
 const StockChart = () => {
   const options = {
-    title: {
-      text: 'My stock chart'
-    },
+   
     series: [
       {
         data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9]
@@ -20,6 +18,7 @@ const StockChart = () => {
       <HighchartsReact
         highcharts={Highcharts}
         constructorType={'stockChart'}
+        containerProps = {{ className: styles.chartContainer }}
         options={options}
       />
       <p>MiFID II compliant pre- and post-trade transaction data is available <a>here</a>. The data is delayed by at least 15 minutes and will be refreshed every 15 minutes. Publication of trades in equity securities that are large in scale compared to normal market size and illiquid non-equity securities may be delayed if the trade is eligible for deferred publication in accordance with the trading rules of Aquis Stock Exchange and MiFIR. Further details as to when the publication of a trade may be deferred are set out in the trading rules which are available <a>here</a>.</p>
