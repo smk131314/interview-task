958d5e84f026d4be0e3bd8d89865266e991eac21
Jade.Kim <jade.kim@travelflan.com>
2022-04-03 14:30:08 +0100

    feat: change stock mock data

--- interview-task/src/components/StockChart.js
+++ interview-task/src/components/StockChart.js
@@ -2,13 +2,14 @@ import { useEffect } from 'react'
 import Highcharts from 'highcharts/highstock'
 import HighchartsReact from 'highcharts-react-official'
 import styles from '../styles/StockChart.module.scss'
+import { MOCK_STOCK_DATA } from '../constants'
 
 const StockChart = () => {
   const options = {
-   
     series: [
       {
-        data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9]
+        data: MOCK_STOCK_DATA,
+        color: '#FF9936'
       }
     ]
   }
